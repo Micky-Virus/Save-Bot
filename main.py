56b26730b71b4e4552b10380475944ad4a709dd3
MICKY <68617397+Micky-Virus@users.noreply.github.com>
2025-08-02 20:57:33 +0530

    main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("👑 OWNER", url="https://t.me/iammicky")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
@@ -249,37 +249,35 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
 		return "Text"
 	except: pass
 
+Powered By :- @DoraBot_Network
+🚀 USAGE GUIDE – Send Telegram Posts Easily
+📢 FOR PUBLIC CHATS
+🟢 Just send the post(s) link.
 
-USAGE = """**FOR PUBLIC CHATS**
+https://t.me/channel_username/1234
 
-__just send post/s link__
+🔐 FOR PRIVATE CHATS
+1️⃣ First, send the invite link of the chat
+(Skip if the account is already a member)
+2️⃣ Then send the post(s) link.
 
-**FOR PRIVATE CHATS**
+Invite: https://t.me/+xyzxyzxyz  
+Post: https://t.me/c/xxxx/101
 
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
+🤖 FOR BOT CHATS
+📎 Send the post link in the following format:
 
-**FOR BOT CHATS**
 
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
-
-```
 https://t.me/b/botusername/4321
-```
 
-**MULTI POSTS**
+💡 You may need an unofficial Telegram client to get the message ID.
 
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+📚 FOR MULTIPLE POSTS
+📝 Use this format to send multiple messages:
 
-```
-https://t.me/xxxx/1001-1010
 
+https://t.me/channel/1001-1010  
 https://t.me/c/xxxx/101 - 120
-```
-
-__note that space in between doesn't matter__
-"""
-
 
 # infinty polling
 bot.run()
